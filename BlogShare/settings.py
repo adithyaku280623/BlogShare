e2b4907390a11f137ecd51ceedcf826ec46af647
adithya-here-now <adithyaku2003@gmail.com>
2025-10-13 20:58:08 +0530

    Made login page mobile-friendly

--- BlogShare/settings.py
+++ BlogShare/settings.py
@@ -82,14 +82,11 @@ AUTH_USER_MODEL = 'AccountManagingApp.CustomUser'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blogdb',
-        'USER': 'bloguser',
-        'PASSWORD': '1111',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 
